3c133ebeef12e8752434ccee1859668d0933b2ea
tatsu73 <tatsuro.saito73@gmail.com>
2016-10-19 16:08:09 +0900

    UK assets

---
+++ Assets/makeWorld.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class makeWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
